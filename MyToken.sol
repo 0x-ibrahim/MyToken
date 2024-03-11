209330b40dde278e157a176306fea7cebeb58d6d
0x-ibrahim <0xibrahim0.5@gmail.com>
2024-03-11 21:37:23 +0100

    Remove unnecessary ^ character. The caret (^) character is no longer needed in the latest Solidity version.

--- MyToken.sol
+++ MyToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 // Defining the contract name
 contract MyToken {
     mapping (address => uint256) public balanceOf;
